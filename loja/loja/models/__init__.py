ff4139d07fe1d5c6b452fe77242b4e1b4280d58c
gustavomesc <gustavomesquitacavalcante@gmail.com>
2026-04-06 11:32:13 -0300

    atualizações

--- loja/loja/models/__init__.py
+++ loja/loja/models/__init__.py
@@ -4,3 +4,5 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 #acima são bibliotecas padrões necessárias do Django, e abaixonossos models
 from .fabricantes import Fabricante
+from .categoria import Categoria
+from .produto import Produto
