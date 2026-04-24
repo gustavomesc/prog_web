abbf1a5fa1e2db03414c5dad13a97e1264e04cb4
gustavomesc <gustavomesquitacavalcante@gmail.com>
2026-04-24 11:21:42 -0300

    aula 4

--- loja/loja/admin.py
+++ loja/loja/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import * #imporata nossos models
-admin.site.register(Fabricante)
 admin.site.register(Categoria)
-admin.site.register(Produto)
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
+    'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    fields = ('Produto', 'destaque', 'promocao',
+'msgPromocao', 'preco', 'categoria',)
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Fabricante,FabricanteAdmin)
 
 # Register your models here.
