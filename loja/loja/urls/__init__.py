e3bd9779182dbd616da9871bae0eee73b150be1b
gustavomesc <gustavomesquitacavalcante@gmail>
2026-05-04 11:39:19 -0300

    aula 6

--- loja/loja/urls/__init__.py
+++ loja/loja/urls/__init__.py
@@ -1,1 +1,2 @@
 from .HomeUrls import *
+from .ProdutosUrls import *
--- loja/lojaAdmin/urls.py
+++ loja/lojaAdmin/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('loja.urls.HomeUrls')),
+    path('produto/', include('loja.urls.ProdutosUrls')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
