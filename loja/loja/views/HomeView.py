b0eadfdfaff7b91a76ed43fc4dba51b66fefab23
gustavomesc <gustavomesquitacavalcante@gmail>
2026-05-22 11:31:50 -0300

    aula 7

--- loja/loja/views/HomeView.py
+++ loja/loja/views/HomeView.py
@@ -1,3 +1,4 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 def home_view(request):
-    return HttpResponse('<h1>Olá mundo!</h1>')
+    return render(request, template_name='home.html', status=200)
