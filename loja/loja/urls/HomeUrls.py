0c57c44c4135c918bff94dd22682edcf4231776c
gustavomesc <gustavomesquitacavalcante@gmail>
2026-06-01 11:33:41 -0300

    aula 8

--- loja/loja/urls/HomeUrls.py
+++ loja/loja/urls/HomeUrls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from loja.views.HomeView import *
 urlpatterns = [
-path("", home_view),
+path("", home_view,name= 'home'),
 ]
--- loja/loja/views/HomeView.py
+++ loja/loja/views/HomeView.py
@@ -1,4 +1,13 @@
-from django.http import HttpResponse
+from loja.models import Produto
 from django.shortcuts import render
+from datetime import timedelta
+from django.utils import timezone
 def home_view(request):
-    return render(request, template_name='home.html', status=200)
+    produto = request.GET.get("produto")
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)
+    context = {
+            'produtos': produtos
+        }
+    return render(request, template_name='home/home.html', status=200,context=context)
